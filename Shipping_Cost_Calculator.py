67efafcb3f10890eae04acb32d5b494d0b15384d
nangaeindray <nangsaooutte4@gmail.com>
2025-03-07 20:48:09 +0700

    update the shipping cost

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -7,6 +7,9 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 ## Calculate shipping cost
 shipping_cost = weight * rate
 
+exchange_rate = 0.91
+eur_amount = shipping_cost * exchange_rate
+
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-
+print(f"Shipping Cost in EUR is : {shipping_cost} EUR")
